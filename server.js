80a46037fa4d7165486db1691311fa3e8a9d1466
Andrea Dito <andrea.dito@gmail.com>
2020-10-13 21:13:21 +0100

    deploy

---
+++ server.js
@@ -0,0 +1,7 @@
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+
+__importDefault(require('./build/main/index'));
